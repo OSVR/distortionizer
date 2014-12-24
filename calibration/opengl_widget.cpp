ee96f9b250ce13d8946f2eaca4571c78559fdd1f
Russell M. Taylor II <taylorr@cs.unc.edu>
2014-12-24 13:51:58 -0500

    Saves and loads configurations.  Draws a grid and circle targets to help with calibration.  Supports separate calibration coefficients for red, green, and blue subject only to them all being larger than 0 and being sequentially larger than each other.

--- calibration/opengl_widget.cpp
+++ calibration/opengl_widget.cpp
@@ -1,3 +1,21 @@
+/** @file
+    @brief Header
+
+    @date 2014
+
+    @author
+    Russell Taylor
+    <russ@reliasolve.com>
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
 #include <QtGui>
 #include <QtOpenGL>
 #include <QColor>
@@ -18,9 +36,9 @@ OpenGL_Widget::OpenGL_Widget(QWidget *parent)
     : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
     , d_cop_l(QPoint(0,0))
     , d_cop_r(QPoint(0,0))
-    , k1_red(1)
-    , dk_green(1)
-    , dk_blue(1)
+    , d_k1_red(0)
+    , d_k1_green(0)
+    , d_k1_blue(0)
 {
 }
 
@@ -49,34 +67,119 @@ void OpenGL_Widget::initializeGL()
 //    emit newVersionLabel(tr("Version 1.0.0"));
 }
 
-void OpenGL_Widget::drawCircle(QPoint center, float radius)
+// The distortion is with respect to a center of projection, which
+// is common for all three colors.  The first-order correction that
+// can be applied assumes that there is an additional radial shift
+// that is proportionaly to the square of the distance of a pixel
+// from the center of projection, with the coefficient called K1:
+//    RcorrR = Rinit + K1R * (Rinit * Rinit)
+//    RcorrG = Rinit + K1G * (Rinit * Rinit)
+//    RcorrB = Rinit + K1B * (Rinit * Rinit)
+//    0 <= K1R <= K1G <= K1B
+//
+// We can solve for a pre-distorted position that would produce
+// a desired end location after passing through the optical system
+// by solving for Rinit in the above equations (here done for a
+// single color):
+//    K1 * Rinit^2 + Rinit - Rcorr = 0
+//    Solving for the positive root of the quadratic equation
+//       A = K1, B = 1, C = -Rcorr
+//    Rinit = (-B + sqrt(B^2 - 4AC)) / 2A
+//          = (-1 + sqrt(1 + 4*K1*Rcorr)) / (2*K1)
+//    K1 > 0
+
+QPointF OpenGL_Widget::transformPoint(QPointF p, QPoint cop, unsigned color)
 {
+    QPointF ret = p;
+    QPointF offset = p - cop;
+    float r2 = offset.x() * offset.x() + offset.y() * offset.y();
+    float r = sqrt(r2);
+    float k1;
+    switch (color) {
+    case 0:
+        k1 = d_k1_red;
+        break;
+    case 1:
+        k1 = d_k1_green;
+        break;
+    case 2:
+        k1 = d_k1_blue;
+        break;
+    }
+
+    // If K1 <= 0, we don't do anything.
+    // Otherwise, we solve the quadratic equation to determine
+    // the initial offset that would put the point at the
+    // requested (corrected) location.  We the apply this offset
+    // to the center of projection to determine the point to plot
+    // that will result in the point landing where we want after
+    // passing through the lens system.
+    if (k1 > 0) {
+        float Rinit = (-1 + sqrt(1 + 4 * k1 * r))/(2*k1);
+        QPointF offset_dir = offset / r;
+        ret = cop + Rinit * offset_dir;
+    }
+
+    return ret;
+}
+
+void OpenGL_Widget::drawCorrectedLine(QPoint begin, QPoint end,
+                                      QPoint cop, unsigned color)
+{
+    QPointF offset = end - begin;
+    float len = sqrt(offset.x() * offset.x() + offset.y() * offset.y());
+    QPointF offset_dir = offset / len;
     glBegin(GL_LINE_STRIP);
-    float step = 1 / radius;
-    for (float r = 0; r <= 2*M_PI; r += step) {
-        glVertex2f(center.x() + radius * cos(r),
-                   center.y() + radius * sin(r));
+    for (float s = 0; s <=len; s++) {
+        QPointF p = begin + s*offset_dir;
+        QPointF tp = transformPoint(p, cop, color);
+        glVertex2f(tp.x(), tp.y());
     }
     glEnd();
 }
 
-void OpenGL_Widget::paintGL()
+void OpenGL_Widget::drawCorrectedCircle(QPoint center, float radius,
+                                      QPoint cop, unsigned color)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    glTranslatef(0.0, 0.0, -10.0);
+    glBegin(GL_LINE_STRIP);
+    float step = 1 / radius;
+    for (float r = 0; r <= 2*M_PI; r += step) {
+        QPointF p(center.x() + radius * cos(r),
+                   center.y() + radius * sin(r));
+        QPointF tp = transformPoint(p, cop, color);
+        glVertex2f(tp.x(), tp.y());
+    }
+    glEnd();
+}
 
-    // Set up rendering state.
-    glDisable(GL_TEXTURE_2D);
+void OpenGL_Widget::drawCorrectedLines(QPoint begin, QPoint end, QPoint cop)
+{
+    float bright = 0.5f;
+    glColor3f(bright, 0.0, 0.0);
+    drawCorrectedLine(begin, end, cop, 0);
 
-    // Turn on blending, so that we'll get white
-    // lines when we draw three different-colored lines
-    // in the same location.  Also turn off Z-buffer
-    // test so we get all of the lines drawn.
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_ONE, GL_ONE);
-    glDisable(GL_DEPTH_TEST);
+    glColor3f(0.0, bright, 0.0);
+    drawCorrectedLine(begin, end, cop, 1);
 
+    glColor3f(0.0, 0.0, bright);
+    drawCorrectedLine(begin, end, cop, 2);
+}
+
+void OpenGL_Widget::drawCorrectedCircles(QPoint center, float radius, QPoint cop)
+{
+    float bright = 0.5f;
+    glColor3f(bright, 0.0, 0.0);
+    drawCorrectedCircle(center, radius, cop, 0);
+
+    glColor3f(0.0, bright, 0.0);
+    drawCorrectedCircle(center, radius, cop, 1);
+
+    glColor3f(0.0, 0.0, bright);
+    drawCorrectedCircle(center, radius, cop, 2);
+}
+
+void OpenGL_Widget::drawCrossHairs()
+{
     // Draw two perpendicular lines through the center of
     // projection on the left eye, and the right eye.
     glColor3f(1.0, 1.0, 1.0);
@@ -91,27 +194,99 @@ void OpenGL_Widget::paintGL()
         glVertex2f(d_cop_r.x(), 0);
         glVertex2f(d_cop_r.x(), d_height);
     glEnd();
+}
 
-    // Draw three colored circles around the center of projection
-    // that go most of the way across each half of the screen.  The
-    // spacing is determined by the dk_green and dk_blue factors.
-    // Because we're using blending, this will result in a white
-    // circle when all three have the same radius and there is no
-    // lens color aberration.
-    float r_red = 0.75 * d_width/4;
-    float r_green = r_red * dk_green;
-    float r_blue = r_red * dk_blue;
-    glColor3f(1.0, 0.0, 0.0);
-    drawCircle(d_cop_l, r_red);
-    drawCircle(d_cop_r, r_red);
+void OpenGL_Widget::drawGrid()
+{
+    // Draw a set of vertical grid lines to the right and left
+    // of the center of projection for each eye.  Draw a red,
+    // green, and blue line at each location with less than
+    // full brightness.  Draw from the top of the screen to
+    // the bottom.
+    int spacing = 40;
+    // Vertical lines
+    for (int r = spacing; r < d_width/2; r += spacing) {
+        // Vertical lines, left eye
+        if (d_cop_l.x() + r < d_width/2) {
+            QPoint begin(d_cop_l.x() + r, 0);
+            QPoint end(d_cop_l.x() + r, d_height);
+            drawCorrectedLines(begin, end, d_cop_l);
+        }
+        if (d_cop_l.x() - r >= 0) {
+            QPoint begin(d_cop_l.x() - r, 0);
+            QPoint end(d_cop_l.x() - r, d_height);
+            drawCorrectedLines(begin, end, d_cop_l);
+        }
 
-    glColor3f(0.0, 1.0, 0.0);
-    drawCircle(d_cop_l, r_green);
-    drawCircle(d_cop_r, r_green);
+        // Vertical lines, right eye
+        if (d_cop_r.x() + r < d_width) {
+            QPoint begin(d_cop_r.x() + r, 0);
+            QPoint end(d_cop_r.x() + r, d_height - 1);
+            drawCorrectedLines(begin, end, d_cop_r);
+        }
+        if (d_cop_r.x() - r >= d_width/2) {
+            QPoint begin(d_cop_r.x() - r, 0);
+            QPoint end(d_cop_r.x() - r, d_height - 1);
+            drawCorrectedLines(begin, end, d_cop_r);
+        }
+    }
 
-    glColor3f(0.0, 0.0, 1.0);
-    drawCircle(d_cop_l, r_blue);
-    drawCircle(d_cop_r, r_blue);
+    // Horizontal lines
+    for (int r = spacing; r < d_height; r += spacing) {
+        // Horizontal lines, left eye
+        if (d_cop_l.y() + r < d_height) {
+            QPoint begin(0, d_cop_l.y() + r);
+            QPoint end(d_width/2 - 1, d_cop_l.y() + r);
+            drawCorrectedLines(begin, end, d_cop_l);
+        }
+        if (d_cop_l.y() - r >= 0) {
+            QPoint begin(0, d_cop_l.y() - r);
+            QPoint end(d_width/2-1, d_cop_l.y() - r);
+            drawCorrectedLines(begin, end, d_cop_l);
+        }
+
+        // Horizontal lines, right eye
+        if (d_cop_r.y() + r < d_height) {
+            QPoint begin(d_width/2, d_cop_r.y() + r);
+            QPoint end(d_width - 1, d_cop_r.y() + r);
+            drawCorrectedLines(begin, end, d_cop_r);
+        }
+        if (d_cop_r.y() - r >= 0) {
+            QPoint begin(d_width/2, d_cop_r.y() - r);
+            QPoint end(d_width-1, d_cop_r.y() - r);
+            drawCorrectedLines(begin, end, d_cop_r);
+        }
+    }
+}
+
+void OpenGL_Widget::drawCircles()
+{
+    drawCorrectedCircles(d_cop_l, 0.1 * d_width/4, d_cop_l);
+    drawCorrectedCircles(d_cop_r, 0.1 * d_width/4, d_cop_r);
+    drawCorrectedCircles(d_cop_l, 0.7 * d_width/4, d_cop_l);
+    drawCorrectedCircles(d_cop_r, 0.7 * d_width/4, d_cop_r);
+}
+
+void OpenGL_Widget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, -10.0);
+
+    // Set up rendering state.
+    // Turn on blending, so that we'll get white
+    // lines when we draw three different-colored lines
+    // in the same location.  Also turn off Z-buffer
+    // test so we get all of the lines drawn.  Also,
+    // turn of texture-mapping.
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_TEXTURE_2D);
+
+    drawCrossHairs();
+    drawGrid();
+    drawCircles();
 }
 
 void OpenGL_Widget::resizeGL(int width, int height)
@@ -147,12 +322,20 @@ void OpenGL_Widget::resizeGL(int width, int height)
 
 void OpenGL_Widget::keyPressEvent(QKeyEvent *event)
 {
-    float color_shift_scale = 1 + 1.0 / (d_width/2);
+    // Color shift equivalent to one pixel at the edge of
+    // the screen if the center of projection is in the
+    // middle of the screen.
+    float color_shift = 1.0 / ((d_width/4.0)*(d_width/4.0));
     switch (event->key()) {
     case Qt::Key_S: // Save the state to an output file.
         // XXX Would like to throw a dialog box, but it shows in HMD
         // and cannot be moved.
         saveConfigToJson("OSVR_HMD_config.json");
+        break;
+    case Qt::Key_L: // Load the state from an output file.
+        // XXX Would like to throw a dialog box, but it shows in HMD
+        // and cannot be moved.
+        loadConfigFromJson("OSVR_HMD_config.json");
         break;
     case Qt::Key_Left:
         d_cop_l.setX(d_cop_l.x()-1);
@@ -182,21 +365,37 @@ void OpenGL_Widget::keyPressEvent(QKeyEvent *event)
         // around the screen center to find the right eye's COP.
         d_cop_r = QPoint(d_width - d_cop_l.x(), d_cop_l.y());
         break;
+    case Qt::Key_R:
+        if(event->modifiers() & Qt::ShiftModifier) {
+            d_k1_red -= color_shift;
+        } else {
+            d_k1_red += color_shift;
+        }
+        // Do not break here: Red shift also affects green and blue
+        //break;
     case Qt::Key_G:
         if(event->modifiers() & Qt::ShiftModifier) {
-            dk_green *= color_shift_scale;
+            d_k1_green -= color_shift;
         } else {
-            dk_green /= color_shift_scale;
+            d_k1_green += color_shift;
         }
-        break;
+        // Do not break here: Green shift also affects blue
+        //break;
     case Qt::Key_B:
         if(event->modifiers() & Qt::ShiftModifier) {
-            dk_blue *= color_shift_scale;
+            d_k1_blue -= color_shift;
         } else {
-            dk_blue /= color_shift_scale;
+            d_k1_blue += color_shift;
         }
         break;
     }
+
+    // Enforce color-shift constraints.
+    if (d_k1_red < 0) { d_k1_red = 0; }
+    if (d_k1_green < d_k1_red) { d_k1_green = d_k1_red; }
+    if (d_k1_blue < d_k1_green) { d_k1_blue = d_k1_green; }
+    printf("XXX R = %g, G = %g, B = %g\n", d_k1_red, d_k1_green, d_k1_blue);
+
     updateGL();
 }
 
@@ -211,20 +410,20 @@ void OpenGL_Widget::mousePressEvent(QMouseEvent *event)
         d_cop_r = QPoint(d_width - d_cop_l.x(), d_cop_l.y());
     }
     updateGL();
-//    lastPos = event->pos();
+//    d_last_pos = event->pos();
 }
 
 void OpenGL_Widget::mouseMoveEvent(QMouseEvent *event)
 {
-    //int dx = event->x() - lastPos.x();
-    //int dy = event->y() - lastPos.y();
+    //int dx = event->x() - d_last_pos.x();
+    //int dy = event->y() - d_last_pos.y();
 
     if (event->buttons() & Qt::LeftButton) {
         // XXX
     } else if (event->buttons() & Qt::RightButton) {
         // XXX
     }
-//    lastPos = event->pos();
+//    last_pos = event->pos();
 }
 
 bool OpenGL_Widget::saveConfigToJson(QString filename)
@@ -237,28 +436,117 @@ bool OpenGL_Widget::saveConfigToJson(QString filename)
     }
     fprintf(f, "{\n");
     fprintf(f, " \"hmd\": {\n");
+    fprintf(f, "  \"distortion\": {\n");
+    fprintf(f, "   \"k1_squared_pixels_term_for_red\": %g,\n", d_k1_red);
+    fprintf(f, "   \"k1_squared_pixels_term_for_green\": %g,\n", d_k1_green);
+    fprintf(f, "   \"k1_squared_pixels_term_for_blue\": %g\n", d_k1_blue);
+    fprintf(f, "  },\n");
     fprintf(f, "  \"eyes\": [\n");
     fprintf(f, "    {\n");
-    fprintf(f, "     \"distortion\": {\n");
     fprintf(f, "      \"Center_of_projection_pixels_x\": %d,\n", d_cop_l.x());
-    fprintf(f, "      \"Center_of_projection_pixels_y\": %d,\n", d_cop_l.y());
-    fprintf(f, "      \"k1_squared_term_for_red\": %f,\n", k1_red);
-    fprintf(f, "      \"k1_squared_term_for_green\": %f,\n", k1_red * dk_green);
-    fprintf(f, "      \"k1_squared_term_for_blue\": %f\n", k1_red * dk_blue);
-    fprintf(f, "     }\n");
+    fprintf(f, "      \"Center_of_projection_pixels_y\": %d\n", d_cop_l.y());
     fprintf(f, "    },\n");
     fprintf(f, "    {\n");
-    fprintf(f, "     \"distortion\": {\n");
-    fprintf(f, "      \"Center_of_projection_pixels_x\": %d,\n", d_cop_r.x());
-    fprintf(f, "      \"Center_of_projection_pixels_y\": %d,\n", d_cop_r.y());
-    fprintf(f, "      \"k1_squared_term_for_red\": %f,\n", k1_red);
-    fprintf(f, "      \"k1_squared_term_for_green\": %f,\n", k1_red * dk_green);
-    fprintf(f, "      \"k1_squared_term_for_blue\": %f\n", k1_red * dk_blue);
-    fprintf(f, "     }\n");
+    // Center of projection with respect to the lower-left corner of the frame.
+    fprintf(f, "      \"Center_of_projection_pixels_x\": %d,\n", d_cop_r.x() - d_width/2);
+    fprintf(f, "      \"Center_of_projection_pixels_y\": %d\n", d_cop_r.y());
     fprintf(f, "    }\n");
     fprintf(f, "   ]\n");
     fprintf(f, " }\n");
     fprintf(f, "}\n");
+
+    fclose(f);
+    return true;
+}
+
+bool OpenGL_Widget::loadConfigFromJson(QString filename)
+{
+    FILE *f = fopen(filename.toStdString().c_str(), "r");
+    if (f == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't load from to %s",
+                filename.toStdString().c_str());
+        return false;
+    }
+
+    // Lines and parameters to read
+    char line[1024];
+    char param[1024];
+    float val;
+    int ival;
+
+    // Skip the first three lines.
+    fgets(line, sizeof(line), f);
+    fgets(line, sizeof(line), f);
+    fgets(line, sizeof(line), f);
+
+    // Try and read the red term
+    if (fgets(line, sizeof(line), f) == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't read red line");
+        return false;
+    }
+    if (sscanf(line, "%s %g", param, &val) != 2)  {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Bad red line: %s",
+                line);
+        return false;
+    }
+    d_k1_red = val;
+
+    // Try and read the green term
+    if (fgets(line, sizeof(line), f) == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't read green line");
+        return false;
+    }
+    if (sscanf(line, "%s %g", param, &val) != 2)  {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Bad green line: %s",
+                line);
+        return false;
+    }
+    d_k1_green = val;
+
+    // Try and read the blue term
+    if (fgets(line, sizeof(line), f) == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't read blue line");
+        return false;
+    }
+    if (sscanf(line, "%s %g", param, &val) != 2)  {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Bad blue line: %s",
+                line);
+        return false;
+    }
+    d_k1_blue = val;
+
+    // Skip the next three lines
+    fgets(line, sizeof(line), f);
+    fgets(line, sizeof(line), f);
+    fgets(line, sizeof(line), f);
+
+    // Try and read the left-eye X COP term
+    if (fgets(line, sizeof(line), f) == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't read left COP x line");
+        return false;
+    }
+    if (sscanf(line, "%s %d", param, &ival) != 2)  {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Bad left COP x line: %s",
+                line);
+        return false;
+    }
+    d_cop_l.setX(ival);
+
+    // Try and read the left-eye Y COP term
+    if (fgets(line, sizeof(line), f) == NULL) {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Can't read left COP y line");
+        return false;
+    }
+    if (sscanf(line, "%s %d", param, &ival) != 2)  {
+        fprintf(stderr, "OpenGL_Widget::loadConfigFromJson(): Bad left COP y line: %s",
+                line);
+        return false;
+    }
+    d_cop_l.setY(ival);
+
+    // Find the mirror of the left-eye's center of projection
+    // around the screen center to find the right eye's COP.
+    d_cop_r = QPoint(d_width - d_cop_l.x(), d_cop_l.y());
 
     fclose(f);
     return true;
--- calibration/opengl_widget.h
+++ calibration/opengl_widget.h
@@ -1,5 +1,61 @@
+/** @file
+    @brief Header
+
+    @date 2014
+
+    @author
+    Russell Taylor
+    <russ@reliasolve.com>
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
 #pragma once
 #include <QGLWidget>
+
+// There are three different indices of refraction for the three
+// different wavelengths in the head-mounted display (R, G, B).
+// This is equivalent to having lenses with three different
+// powers, one per color.  This produces effectively three different
+// radial distortion patterns, one per color.  The distortion of
+// red is the least, green next, and blue the most.
+//
+// The relative values (and ratios) of these distortions depends
+// on the particular material that the lenses are made of, and is
+// based on an emperical fit.  This means that we must determine
+// each of them independently, subject to the above inequality
+// constraint.
+//
+// The distortion is with respect to a center of projection, which
+// is common for all three colors.  The first-order correction that
+// can be applied assumes that there is an additional radial shift
+// that is proportionaly to the square of the distance of a pixel
+// from the center of projection, with the coefficient called K1:
+//    RcorrR = Rinit + K1R * (Rinit * Rinit)
+//    RcorrG = Rinit + K1G * (Rinit * Rinit)
+//    RcorrB = Rinit + K1B * (Rinit * Rinit)
+//    0 <= K1R <= K1G <= K1B
+//
+// We can solve for a pre-distorted position that would produce
+// a desired end location after passing through the optical system
+// by solving for Rinit in the above equations (here done for a
+// single color):
+//    K1 * Rinit^2 + Rinit - Rcorr = 0
+//    Solving for the positive root of the quadratic equation
+//       A = K1, B = 1, C = -Rcorr
+//    Rinit = (-B + sqrt(B^2 - 4AC)) / 2A
+//          = (-1 + sqrt(1 + 4*K1*Rcorr)) / (2*K1)
+//    K1 > 0
+//
+// When there are nonzero coefficients for higher-order terms
+// (K2 and above), the result is a fourth-order polynomial that
+// is challenging to invert analytically.
 
 class OpenGL_Widget : public QGLWidget
 {
@@ -14,21 +70,56 @@ public slots:
 signals:
 
 protected:
+    //------------------------------------------------------
     void initializeGL();
     void paintGL();
     void resizeGL(int width, int height);
     void mousePressEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
     void keyPressEvent(QKeyEvent *event);
-    void drawCircle(QPoint center, float radius);
+
+    //------------------------------------------------------
     bool saveConfigToJson(QString filename);
+    bool loadConfigFromJson(QString filename);
+
+    //------------------------------------------------------
+    // Used as options in the rendering, depending on our
+    // mode.
+    void drawCrossHairs();
+    void drawGrid();
+    void drawCircles();
+
+    //------------------------------------------------------
+    // Helper functions for the draw routines.
+
+    /// Draw a line from the specified begin point to the
+    // specified end, doing distortion correcton.  The line
+    // is drawn in short segments, with the correction applied
+    // to each segment endpoint.  The color index tells whether
+    // we use red (0), green (1), or blue (2) correction factors.
+    // It uses the specified center of projection.
+    void drawCorrectedLine(QPoint begin, QPoint end,
+                           QPoint cop, unsigned color);
+    void drawCorrectedCircle(QPoint center, float radius,
+                           QPoint cop, unsigned color);
+
+    /// Draw a set of 3 colored lines from the specified begin
+    // point to the specified end, doing distortion correcton.
+    void drawCorrectedLines(QPoint begin, QPoint end, QPoint cop);
+    void drawCorrectedCircles(QPoint center, float radius, QPoint cop);
+
+    /// Transform the specified pixel coordinate by the
+    // color-correction distortion matrix using the appropriate
+    // distortion correction.  The color index tells whether
+    // we use red (0), green (1), or blue (2) correction factors.
+    // It uses the specified center of projection.
+    QPointF transformPoint(QPointF p, QPoint cop, unsigned color);
 
 private:
     int d_width, d_height;  //< Size of the window we're rendering into
     QPoint d_cop_l;         //< Center of projection for the left eye
     QPoint d_cop_r;         //< Center of projection for the right eye
-    float  k1_red;          //< Quadratic term for distortio of red
-    float  dk_green;        //< Multiplicative factor from red to green
-    float  dk_blue;         //< Multiplicative factor from red to blue
+    float  d_k1_red;        //< Quadratic term for distortion of red
+    float  d_k1_green;      //< Quadratic term for distortion of green
+    float  d_k1_blue;       //< Quadratic term for distortion of blue
 };
-
