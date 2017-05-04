66d21f5cc9fcb895374df02e7df7c5efc8519b96
Ryan Pavlik <ryan@sensics.com>
2017-05-04 08:56:22 -0500

    Add modular/json-driven angles to config frontend.

---
+++ angles_to_config/AnglesToConfigModular.cpp
@@ -0,0 +1,295 @@
+/** @file
+    @brief Implementation
+
+    @date 2017
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2017 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include "GenerateOutput.h"
+#include "JsonUtils.h"
+#include "Process.h"
+#include "helper.h"
+
+// Library/third-party includes
+#include <json/reader.h>
+#include <json/value.h>
+
+// Standard includes
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+using namespace osvr::util;
+/// Returns true and changes outval only if a full set of bounds is found.
+bool getBounds(Json::Value const& obj, RectBoundsd& outVal) {
+    if (!obj.isObject()) {
+        return false;
+    }
+    RectBoundsd ret;
+    {
+        auto& left = obj["left"];
+        if (!json_is<double>(left)) {
+            return false;
+        }
+        ret.left = json_cast<double>(left);
+    }
+
+    {
+        auto& right = obj["right"];
+        if (!json_is<double>(right)) {
+            return false;
+        }
+        ret.right = json_cast<double>(right);
+    }
+    {
+        auto& top = obj["top"];
+        if (!json_is<double>(top)) {
+            return false;
+        }
+        ret.top = json_cast<double>(top);
+    }
+    {
+        auto& bottom = obj["bottom"];
+        if (!json_is<double>(bottom)) {
+            return false;
+        }
+        ret.bottom = json_cast<double>(bottom);
+    }
+    outVal = ret;
+    return true;
+}
+
+void basicConfigParsing(Json::Value const& root, Config& conf) {
+    conf.depth = getWithDefault(root, "depth", conf.depth);
+    {
+        auto& screen = root["screen"];
+        auto gotBounds = getBounds(screen, conf.suppliedScreenBounds);
+        if (gotBounds) {
+            conf.computeScreenBounds = false;
+        }
+    }
+    {
+        auto& inputType = root["inputType"];
+        if (json_is<std::string>(inputType)) {
+            auto inputTypeStr = json_cast<std::string>(inputType);
+            if (inputTypeStr == "fieldAngles") {
+                conf.useFieldAngles = true;
+            } else if (inputTypeStr == "latitudeLongitude") {
+                conf.useFieldAngles = false;
+            } else {
+                std::cerr << "Warning: Unrecognized value for 'inputType'" << std::endl;
+            }
+        }
+    }
+    /// @todo verify angles
+    conf.verbose = getWithDefault(root, "verbose", conf.verbose);
+}
+
+bool supplySingleFileData(std::string const& fn, AnglesToConfigSingleEyeProcess& process) {
+    std::ifstream inFile(fn);
+    if (!inFile.good()) {
+        std::cerr << "Error: Could not open file " << fn << std::endl;
+        return false;
+    }
+    auto mapping = read_from_infile(inFile);
+    if (mapping.empty()) {
+        std::cerr << "Error: Failure to read records from " << fn << std::endl;
+        return false;
+    }
+    auto success = process.supplyInputMapping(std::move(mapping));
+    if (0 != success) {
+        std::cerr << "Error: supplyInputMapping failed, return value: " << success << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool attemptSingleEyeProcessing(Json::Value const& inputData, AnglesToConfigSingleEyeProcess& process) {
+    auto withJsonSchemaError = [] {
+        std::cerr << "Warning: Elements of 'input' must either be a string or an object with red, green, blue as keys."
+                  << std::endl;
+        return false;
+    };
+    if (inputData.isNull()) {
+        return false;
+    } else if (json_is<std::string>(inputData)) {
+        // OK this is a mono channel.
+        auto fn = json_cast<std::string>(inputData);
+        if (!supplySingleFileData(fn, process)) {
+            return false;
+        }
+    } else if (inputData.isObject()) {
+        for (auto& channel : {"red", "green", "blue"}) {
+            auto chanFnVal = inputData[channel];
+            if (!json_is<std::string>(chanFnVal)) {
+                return withJsonSchemaError();
+            }
+            if (!supplySingleFileData(json_cast<std::string>(chanFnVal), process)) {
+                return false;
+            }
+        }
+    } else {
+        return withJsonSchemaError();
+    }
+    // OK, if we made it here we have at least one file loaded in properly.
+    process.computeBounds();
+    process.normalizeMappings();
+
+    return true;
+}
+
+int outputClientMeshData(std::ostream& os, SingleEyeOutput const& left, SingleEyeOutput const& right) {
+    //====================================================================
+    // Construct Json screen description.
+    // We do this by hand rather than using JsonCPP because we need
+    // to control the printed precision of the numbers to avoid making
+    // a huge file.
+    os << "{" << std::endl;
+    os << R"( "display": {)" << std::endl;
+    os << R"(  "hmd": {)" << std::endl;
+
+    os << R"(   "field_of_view": {)" << std::endl;
+    os << R"(    "monocular_horizontal": )" << right.screen.hFOVDegrees << "," << std::endl;
+    os << R"(    "monocular_vertical": )" << right.screen.vFOVDegrees << "," << std::endl;
+    os << R"(    "overlap_percent": )" << right.screen.overlapPercent << "," << std::endl;
+    os << R"(    "pitch_tilt": 0)" << std::endl;
+    os << "   }," << std::endl; // field_of_view
+
+    os << R"(   "eyes": [)" << std::endl;
+    os << "    {" << std::endl;
+    os << R"(     "center_proj_x": )" << left.screen.xCOP << "," << std::endl;
+    os << R"(     "center_proj_y": )" << left.screen.yCOP << "," << std::endl;
+    os << R"(     "rotate_180": 0)" << std::endl;
+    os << "    }," << std::endl;
+    os << "    {" << std::endl;
+    os << R"(     "center_proj_x": )" << right.screen.xCOP << "," << std::endl;
+    os << R"(     "center_proj_y": )" << right.screen.yCOP << "," << std::endl;
+    os << R"(     "rotate_180": 0)" << std::endl;
+    os << "    }" << std::endl;
+    os << "   ]," << std::endl; // eyes
+
+    os << R"(   "distortion": {)" << std::endl;
+    switch (left.meshes.size()) {
+    case 1:
+        os << R"(    "type": "mono_point_samples",)" << std::endl;
+        os << R"(    "mono_point_samples": [)" << std::endl;
+        writeMesh(os, left.meshes[0]);
+        os << "," << std::endl;
+        writeMesh(os, right.meshes[0]);
+        os << "    ]" << std::endl; // mono_point_samples
+        os << "   }" << std::endl;  // distortion
+        break;
+    case 3:
+        os << R"(    "type": "rgb_point_samples",)" << std::endl;
+        os << R"(    "red_point_samples": [)" << std::endl;
+        writeMesh(os, left.meshes[0]);
+        os << "," << std::endl;
+        writeMesh(os, right.meshes[0]);
+        os << "    ]," << std::endl; // red_point_samples
+        os << R"(    "green_point_samples": [)" << std::endl;
+        writeMesh(os, left.meshes[1]);
+        os << "," << std::endl;
+        writeMesh(os, right.meshes[1]);
+        os << "    ]," << std::endl; // green_point_samples
+        os << R"(    "blue_point_samples": [)" << std::endl;
+        writeMesh(os, left.meshes[2]);
+        os << "," << std::endl;
+        writeMesh(os, right.meshes[2]);
+        os << "    ]" << std::endl; // blue_point_samples
+        os << "   }" << std::endl;  // distortion
+        break;
+    default:
+        std::cerr << "Error: Unexpected number of meshes: " << left.meshes.size() << std::endl;
+        return 3;
+    }
+
+    os << "  }" << std::endl; // hmd
+    os << " }" << std::endl;  // display
+    os << "}" << std::endl;   // Closes outer object
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    Json::Value root;
+    {
+        Json::Reader reader;
+        std::ifstream infile(argv[1]);
+        if (!reader.parse(infile, root)) {
+            std::cerr << "Error parsing config file " << argv[1] << "!" << std::endl;
+            return -1;
+        }
+    }
+    Config conf;
+    basicConfigParsing(root, conf);
+
+    auto& input = root["input"];
+    AnglesToConfigSingleEyeProcess leftEyeProcess(conf);
+    auto haveLeft = attemptSingleEyeProcessing(input["left"], leftEyeProcess);
+
+    AnglesToConfigSingleEyeProcess rightEyeProcess(conf);
+    auto haveRight = attemptSingleEyeProcessing(input["right"], rightEyeProcess);
+    if (!haveLeft && !haveRight) {
+        std::cerr << "Error: must have valid input data for at least one eye, and none provided." << std::endl;
+        return -1;
+    } else if (haveLeft && !haveRight) {
+        std::cerr << "Note: Provided only left eye, assuming right is symmetrical." << std::endl;
+        rightEyeProcess = leftEyeProcess.reflectedHorizontally();
+    } else if (haveRight && !haveLeft) {
+        std::cerr << "Note: Provided only right eye, assuming left is symmetrical." << std::endl;
+        leftEyeProcess = rightEyeProcess.reflectedHorizontally();
+    } else {
+        assert(haveLeft && haveRight && "only remaining option");
+        std::cerr << "Note: Data provided for both eyes." << std::endl;
+    }
+
+    SingleEyeOutput leftOutput;
+    {
+        auto success = leftEyeProcess.computeScreenAndMeshes(leftOutput);
+        if (0 != success) {
+            std::cerr << "Error: Failure in computeScreenAndMeshes for left eye, code " << success << std::endl;
+            return success;
+        }
+    }
+
+    SingleEyeOutput rightOutput;
+    {
+        auto success = rightEyeProcess.computeScreenAndMeshes(rightOutput);
+        if (0 != success) {
+            std::cerr << "Error: Failure in computeScreenAndMeshes for right eye, code " << success << std::endl;
+            return success;
+        }
+    }
+
+    if (rightOutput.meshes.size() != leftOutput.meshes.size()) {
+        std::cerr << "Error: Mismatch in number of meshes between eyes - either both are monochromatic or both are RGB"
+                  << std::endl;
+        return -1;
+    }
+    {
+        auto success = outputClientMeshData(std::cout, leftOutput, rightOutput);
+        if (0 != success) {
+            std::cerr << "Error: failure in outputClientMeshData: " << success << std::endl;
+            return success;
+        }
+    }
+    std::cerr << "Processing complete!" << std::endl;
+    return 0;
+}
---
+++ angles_to_config/JsonUtils.h
@@ -0,0 +1,175 @@
+/** @file
+    @brief Header
+
+    Based on code from GetOptionalParameter in OSVR.
+
+    @date 2017
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2017 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_JsonUtils_h_GUID_F3BC6DA9_BF2B_49A5_A324_8E3A999758DC
+#define INCLUDED_JsonUtils_h_GUID_F3BC6DA9_BF2B_49A5_A324_8E3A999758DC
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+#include <json/value.h>
+
+// Standard includes
+#include <array>
+#include <string>
+
+namespace osvr {
+namespace util {
+    namespace detail {
+        template <typename T> struct JsonTypeGetter;
+        template <typename T> struct JsonTypeContainment;
+#define OSVR_DECLARE_JSON_TYPE_TRAITS(TYPENAME, GET_METHOD, IS_METHOD)                                                 \
+    template <> struct JsonTypeGetter<TYPENAME> {                                                                      \
+        static TYPENAME apply(Json::Value const& val) { return val.GET_METHOD(); }                                     \
+    };                                                                                                                 \
+    template <> struct JsonTypeContainment<TYPENAME> {                                                                 \
+        static bool apply(Json::Value const& val) { return val.IS_METHOD(); }                                          \
+    };
+        OSVR_DECLARE_JSON_TYPE_TRAITS(bool, asBool, isBool)
+        OSVR_DECLARE_JSON_TYPE_TRAITS(float, asFloat, isDouble)
+        OSVR_DECLARE_JSON_TYPE_TRAITS(double, asDouble, isDouble)
+        OSVR_DECLARE_JSON_TYPE_TRAITS(int, asInt, isInt)
+        OSVR_DECLARE_JSON_TYPE_TRAITS(unsigned int, asUInt, isUInt)
+        OSVR_DECLARE_JSON_TYPE_TRAITS(std::string, asString, isString)
+
+#undef OSVR_DECLARE_JSON_TYPE_TRAITS
+    } // namespace detail
+    template <typename T> inline T json_cast(Json::Value const& val) { return detail::JsonTypeGetter<T>::apply(val); }
+    /* End of code from GetOptionalParameter */
+    template <typename T> inline bool json_is(Json::Value const& val) {
+        return detail::JsonTypeContainment<T>::apply(val);
+    }
+
+    template <typename T> inline T getWithDefault(Json::Value const& root, const char* memName, T def) {
+        const bool wasMember = root.isMember(memName);
+        auto& elt = root[memName];
+        if (wasMember) {
+            if (json_is<T>(elt)) {
+                return json_cast<T>(elt);
+            }
+        }
+        return def;
+    }
+
+    template <typename T> inline T getIntrusiveDefault(Json::Value& root, const char* memName, T def) {
+        const bool wasMember = root.isMember(memName);
+        auto& elt = root[memName];
+        if (wasMember) {
+            if (json_is<T>(elt)) {
+                return json_cast<T>(elt);
+            }
+        }
+        elt = def;
+        return def;
+    }
+
+    template <typename T> inline T getIntrusiveNonzeroDefault(Json::Value& root, const char* memName, T def) {
+        const bool wasMember = root.isMember(memName);
+        auto& elt = root[memName];
+        if (wasMember) {
+            if (json_is<T>(elt)) {
+                auto ret = json_cast<T>(elt);
+                if (ret == 0) {
+                    elt = def;
+                    ret = def;
+                }
+                return ret;
+            }
+        }
+        elt = def;
+        return def;
+    }
+    template <typename T, std::size_t N>
+    inline std::array<T, N> getIntrusiveDefaultArray(Json::Value& root, const char* memName,
+                                                     std::array<T, N> const& def) {
+        std::array<T, N> ret;
+        auto& elt = root[memName];
+        bool success = false;
+        if (elt.isArray() && elt.size() == N) {
+            success = true;
+            for (Json::Value::ArrayIndex i = 0; i < N; ++i) {
+                auto& member = elt[i];
+                if (!json_is<T>(member)) {
+                    success = false;
+                    break;
+                }
+                ret[i] = json_cast<T>(elt[i]);
+            }
+        }
+        if (success) {
+            return ret;
+        }
+        /// Wasn't successful in loading, will set instead.
+        elt.resize(N);
+        for (Json::Value::ArrayIndex i = 0; i < N; ++i) {
+            elt[i] = def[i];
+        }
+        return def;
+    }
+
+    template <typename T, std::size_t N, typename F>
+    inline std::array<T, N> getIntrusiveDefaultArrayWithValidity(Json::Value& root, const char* elemName,
+                                                                 std::array<T, N> const& defaultValue,
+                                                                 F elementValidityPredicate) {
+        std::array<T, N> ret;
+        if (root.isMember(elemName)) {
+            auto& elt = root[elemName];
+            if (elt.isArray() && elt.size() == N) {
+                bool didFail = false;
+                for (Json::ArrayIndex i = 0; i < N; ++i) {
+                    auto& entry = elt[i];
+                    if (!json_is<T>(entry)) {
+                        // error in parsing, break before completion
+                        didFail = true;
+                        break;
+                    } else if (!elementValidityPredicate(entry)) {
+                        // user supplied predicate said no
+                        didFail = true;
+                        break;
+                    } else {
+                        // no failure so far.
+                        ret[i] = json_cast<T>(entry);
+                    }
+                }
+                // at end of for loop: did we make it through without failure?
+                if (!didFail) {
+                    return ret;
+                }
+            }
+        }
+        // Somehow not valid - we'll put in the default value instead.
+        ret = defaultValue;
+        auto& elt = root[elemName];
+        elt = Json::Value(Json::arrayValue);
+        for (Json::ArrayIndex i = 0; i < N; ++i) {
+            elt[i] = ret[i];
+        }
+        return ret;
+    }
+} // namespace util
+} // namespace osvr
+#endif // INCLUDED_JsonUtils_h_GUID_F3BC6DA9_BF2B_49A5_A324_8E3A999758DC
